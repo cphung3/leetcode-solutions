0f291b99e7aa08397fbda17ffbbf93a53011dc91
Calvin Phung <calvinphung0@gmail.com>
2022-04-11 22:54:26 -0400

    91

---
+++ dp/91. Decode Ways.py
@@ -0,0 +1,43 @@
+from distutils.command.build import build
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        alpha = 'abcdefghijklmnopqrstuvwxyz'
+        mapping = {str(i): alpha[i] for i in range(len(alpha))}
+        dp = {}
+        res = []
+
+        def recurse(idx, buildup):
+            if idx >= len(s) - 1:
+                res.append(buildup)
+                return
+            # if idx == len(s) - 1:
+            #     return
+            
+            # dp[idx] = buildup
+            if idx + 1 <= len(s) - 1:
+                str1 = mapping.get(s[idx: idx+1])
+                if str1:
+                    recurse(idx+1, buildup + str1)
+            if idx + 2 <= len(s) - 1:
+                str2 = mapping.get(s[idx: idx+2])
+                if str2:
+                    recurse(idx+2, buildup + str2)
+        recurse(0, res)
+        return res
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        if s[0] != '0': 
+            dp[1] = 1
+        for i in range(2, len(s)+1):
+            if int(s[i-1]) >= 1 and int(s[i-1]) <= 9:
+                dp[i] += dp[i-1]
+            second = int(s[i-2:i])
+            if second >= 10 and second <= 26:
+                dp[i] += dp[i-2]
+        return dp[-1]
