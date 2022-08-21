409d6f382ca7d1fddad16f2d98dc133b87573852
Calvin Phung <calvinphung0@gmail.com>
2022-08-21 17:47:02 -0400

    unique paths

--- dp/62. Unique Paths.py
+++ dp/62. Unique Paths.py
@@ -1,3 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for i in range(m)] for j in range(n)]
+        dp[n-1][m-1] = 1
         
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if i + 1 < n and j + 1 < m:
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1]
+                elif i + 1 >= n and j + 1 >= m:
+                    pass
+                elif i + 1 >= n:
+                    dp[i][j] = dp[i][j+1]
+                elif j + 1 >= m:
+                    dp[i][j] = dp[i+1][j]
+        return dp[0][0]
+
+# Runtime: 39 ms, faster than 79.00% of Python3 online submissions for Unique Paths.
+# Memory Usage: 13.8 MB, less than 74.78% of Python3 online submissions for Unique Paths.
