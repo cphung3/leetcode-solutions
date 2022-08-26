f8a0881be836cfec28923cb9fb4f764c90bd4b7f
Calvin Phung <calvinphung0@gmail.com>
2022-08-26 17:54:34 -0400

    revisit two sum

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -16,3 +16,18 @@ class Solution:
                 ans1, ans2 = numDict[diff][0], numDict[val][0]
                 if ans1 != ans2:
                     return [ans1, ans2] 
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevDict = {}
+
+        for idx, val in enumerate(nums):
+            diff = target - val
+            if diff in prevDict:
+                return [prevDict[diff], idx]
+            else:
+                prevDict[val] = idx
+        return []                
+
+# Runtime: 63 ms, faster than 95.29% of Python3 online submissions for Two Sum.
+# Memory Usage: 15.1 MB, less than 50.22% of Python3 online submissions for Two Sum.
+                
