1ed16de1d6efa25aa0bad7b8ece0d28b4a79b6af
Calvin Phung <calvinphung0@gmail.com>
2022-09-16 18:44:38 -0400

    55 finished

--- Blind-75/55. Jump Game.py
+++ Blind-75/55. Jump Game.py
@@ -1,3 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
+        goal = len(nums)-1
+        for i in range(goal-1, -1, -1):
+            if nums[i] >= goal - i:
+                goal = i
+
+        return True if goal == 0 else False
+
+# Runtime: 984 ms, faster than 30.33% of Python3 online submissions for Jump Game.
+# Memory Usage: 15.3 MB, less than 18.22% of Python3 online submissions for Jump Game.
