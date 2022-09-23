4524b1eb7472e71c6f8d323133bcd9974e495034
Calvin Phung <calvinphung0@gmail.com>
2022-09-22 21:31:50 -0400

    longest palindromic substring attempt 2

--- dp/5. Longest Palindromic Substring.py
+++ dp/5. Longest Palindromic Substring.py
@@ -20,3 +20,34 @@ class Solution:
 # https://zkf85.github.io/2019/03/26/leetcode-005-longest-palindrome#solution-3
 # Runtime: 7369 ms, faster than 9.49% of Python3 online submissions for Longest Palindromic Substring.
 # Memory Usage: 14 MB, less than 81.78% of Python3 online submissions for Longest Palindromic Substring.
+class Solution:
+    
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        # Treat every character as a possible center 
+        # and expand outward from that center
+        for i in range(len(s)):
+            l, r = i, i
+
+            # odd case, aba
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even case, abba
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        return res
+
+# Runtime: 701 ms, faster than 81.08% of Python3 online submissions for Longest Palindromic Substring.
+# Memory Usage: 13.9 MB, less than 89.82% of Python3 online submissions for Longest Palindromic Substring.
